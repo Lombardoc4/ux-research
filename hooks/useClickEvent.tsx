c129cd4ef4aab89439c5900bccee9c76b777968b
Cris Lom <bigc4997@gmail.com>
2022-11-09 12:05:27 -0500

    adding interaction metrics

--- hooks/useClickEvent.tsx
+++ hooks/useClickEvent.tsx
@@ -1,12 +1,36 @@
+import { type } from "os";
 
 interface Event {
     tag: string,
-    type: string
+    type: string,
+    time: number,
+    // user: string,
 }
 
-export function eventTracker(event: Event) {
+export async function eventTracker(event: Event) {
+    const prevUser = window.localStorage.getItem('lom13-exp1')
+    
+    console.log('event', {
+        tag: event.tag,
+        type: event.type,
+        timing: (new Date().getTime() - event.time)/1000,
+        user: (prevUser || 'U-' + event.time),
+    })
     // Send data to db
-    console.log('event', event)
+    const confirmResult = true;
+    // const confirmResult = await DataStore.save(
+    //     new Exp1({
+    //         "tag": event.tag,
+    //         "type": event.type,
+    //         "timing": (new Date().getTime() - event.time)/1000,
+    //         "user": (prevUser || 'U-' + event.time),
+    //     })
+    // );
+    
+    
+    // Set Cookie/Local Storage
+    if (!prevUser && confirmResult) 
+        window.localStorage.setItem('lom13-exp1', 'U-' + event.time);
     
     // return confirmation
     
